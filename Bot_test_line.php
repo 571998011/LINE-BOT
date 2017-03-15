14834ad1fb9fc5c08f8f27a9e7152bb8b6a5f727
571998011 <thanapoom.son@crru.ac.th>
2017-03-15 11:46:55 +0700

    Update Bot_test_line.php

--- Bot_test_line.php
+++ Bot_test_line.php
@@ -1,8 +1,8 @@
 <?php
 
-$access_token = tYO69OAFrf1t8TOcRLmVCjqdaMLx3fMXgm4YGlvANWE56EjBjolij67ND422KmII/IXz+YfqhRg9+0vfIHiMsgYsUeHy0H5O6mxOiKbXmXRc3QMfBN2a57IVy/kfJDpT2aWNRtTJ3qMrkQvHcAkb0wdB04t89/1O/w1cDnyilFU=;
-$proxy = dry-woodland-30767;
-$proxyauth = dry-woodland-3076>;
+$access_token = 'tYO69OAFrf1t8TOcRLmVCjqdaMLx3fMXgm4YGlvANWE56EjBjolij67ND422KmII/IXz+YfqhRg9+0vfIHiMsgYsUeHy0H5O6mxOiKbXmXRc3QMfBN2a57IVy/kfJDpT2aWNRtTJ3qMrkQvHcAkb0wdB04t89/1O/w1cDnyilFU=';
+//$proxy = dry-woodland-30767;
+//$proxyauth = dry-woodland-3076>;
 
 // Get POST body content
 $content = file_get_contents('php://input');
